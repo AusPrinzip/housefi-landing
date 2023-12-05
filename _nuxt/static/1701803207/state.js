21b5d71fed2de70363c87fd4337df6b2f76f8a54
AusPrinzip <AusPrinzip@users.noreply.github.com>
2023-12-05 19:07:08 +0000

    deploy: 1a99d53c10ebf70283b2e1d2ed58463dc0024420

--- _nuxt/static/1701803207/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhousefi-landing\u002F_nuxt\u002Fstatic\u002F1701803207",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhousefi-landing\u002F",assetsPath:"\u002Fhousefi-landing\u002F_nuxt\u002F",cdnURL:a}}}}(null));
